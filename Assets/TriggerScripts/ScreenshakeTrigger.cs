a47a96c509c9c164e766f2a31e9442e1fc6e17c0
Lucas Sharkey-Chouinard <lucas@theshards.ca>
2023-11-28 12:31:27 -0400

    Started Screenshake Trigger

---
+++ Assets/TriggerScripts/ScreenshakeTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ScreenshakeTrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+            Debug.Log("Played Timeline");
+        }
+    }
+}
