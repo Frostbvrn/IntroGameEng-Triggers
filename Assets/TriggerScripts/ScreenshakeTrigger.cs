f097daeee476c058188bfa193d624fc26aee3288
Lucas Sharkey-Chouinard <lucas@theshards.ca>
2023-11-29 12:15:19 -0400

    Created first trigger
    Added screenshake trigger

--- Assets/TriggerScripts/ScreenshakeTrigger.cs
+++ Assets/TriggerScripts/ScreenshakeTrigger.cs
@@ -9,7 +9,7 @@ public class ScreenshakeTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "PlayerTag")
         {
             timeline.Play();
             Debug.Log("Played Timeline");
